2123845eed7ffde4a17de81a3710037b72cb73cf
NhatLe2002 <133828098+NhatLe2002@users.noreply.github.com>
2024-10-29 13:36:37 +0700

    Add files via upload

---
+++ Demo/HelloWordConsoleApp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	 cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
+
---
+++ Demo/Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using ll == long long;
+int cnt[1000002];
+
+
+int main(){
+    int TC; cin >> TC;
+    
+}
